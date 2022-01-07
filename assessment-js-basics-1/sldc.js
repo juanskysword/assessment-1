70043756292ecbae1fbeba68c03de0ce1093db76
Juan Tavera <juanskysword@gmail.com>
2022-01-07 10:36:12 -0800

    SLDC completed

--- assessment-js-basics-1/sldc.js
+++ assessment-js-basics-1/sldc.js
@@ -11,11 +11,61 @@
 // Maintain
 // Planning
 // Testing & Integration
+{
+console.log("Steps of the Sofware Development Lifecycle");
 
-
+    console.log  ("  1. Planning");
+    console.log ("--------------")
+    console.log  ('  2. Analysis of Requirements')
+    console.log ("--------------")
+    console.log  ('  3. Design')
+    console.log ("--------------")
+    console.log  ('  4. Implementation')
+    console.log ("--------------")
+    console.log  ('  5. Testing ')
+    console.log ("--------------")
+    console.log  ('  6. Integration')
+    console.log ("--------------")
+    console.log  ('  7. Maintain')
+    console.log ("--------------")
+}
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+{
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." ;
+    console.log( "Planning means ",planning); 
+    console.log ("--------------");
+}
+{
+const alysis = "gathering ideas and details for the first prototype, performing research and analysis to determine users needs. list of specification for the software.";
+    console.log("Analysis of Requirements means",alysis);
+    console.log ("--------------");
+}
+{
+const design ="first outline the detail for the overall application alongside specific aspects, a series of stages of cycle layout for implementation moving foward.";
+    console.log("Design means", design);
+    console.log ("--------------");
+}
+{
+const implementation ="Where the code is written, Developers will write code and build the product following the steps layout for them moving them relatively straightfoward";
+    console.log("Implementation means", implementation);
+    console.log ("--------------");
+}   
+{
+const testing ="Testing phase will go deep into tracking any bugs or defects that can be fixed and tested later, this helps the end-user experince not be negative for when its done.";
+    console.log(testing);
+    console.log ("--------------");
+}    
+{
+const intergration="Information system will be integrated into its envirnment and eventually installed, theoretically ready for market and open to any end-user.";
+    console.log("Intergration means", intergration);
+    console.log ("--------------");
+}    
+{
+const maintain="The Tracking of reports, bugs or any issues handle by end-users, implementing any changes for future content and handling new bugs.";
+    console.log("Maintain means", maintain);
+    console.log ("--------------");
+}    
