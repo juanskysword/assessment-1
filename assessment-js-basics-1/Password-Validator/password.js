5072677df7f362af8fae45c39d118e7e8a3ca9fc
Juan Tavera <juanskysword@gmail.com>
2022-01-07 12:29:17 -0800

    stuck on password

---
+++ assessment-js-basics-1/Password-Validator/password.js
@@ -0,0 +1,10 @@
+//password
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+reader.question("Welcome User to the password validator tool",
